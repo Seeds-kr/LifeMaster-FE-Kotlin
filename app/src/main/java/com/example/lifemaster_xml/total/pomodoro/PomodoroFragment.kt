b4c11af993375901480785eca86272bbeaf48751
SEYOUNG PARK <seyeongb18@gmail.com>
2024-11-09 12:37:21 +0900

    [feat]: 포모도로 시작하기 클릭 시 타이머 작동
    포모도로 시간 타이머 설정
    - 시작 버튼 클릭 시 타이머 흐름
    
    구현 방법)
    main thread 가 아닌 worker thread 에서 진행해야 한다. 왜냐하면, 메인쓰레드에서 작업하면 타이머가 돌아갈 때 다른 ui 가 작동하지 않아 ANR 오류가 발생하기 때문이다.
    
    쓰레드 생성 방법 1) Thread { .. }
    쓰레드 생성 방법 2) timer → 해당 방법 사용

--- app/src/main/java/com/example/lifemaster_xml/total/pomodoro/PomodoroFragment.kt
+++ app/src/main/java/com/example/lifemaster_xml/total/pomodoro/PomodoroFragment.kt
@@ -1,15 +1,17 @@
 package com.example.lifemaster_xml.total.pomodoro
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.lifemaster_xml.R
 import com.example.lifemaster_xml.data.Datas
 import com.example.lifemaster_xml.databinding.FragmentPomodoroBinding
+import java.util.*
+import kotlin.concurrent.timer
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,6 +32,9 @@ class PomodoroFragment : Fragment() {
     private val sharedViewModel by lazy {
         ViewModelProvider(requireActivity()).get(PomodoroViewModel::class.java) // https://black-jin0427.tistory.com/389
     }
+    private var totalSecond = 0
+
+    private var timer: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,17 +54,110 @@ class PomodoroFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+        observeViewModel()
+    }
+
+    fun setupListeners() {
         binding.ivOpenTodoDialog.setOnClickListener {
             val dialog = PomodoroDialog()
             dialog.isCancelable = false
             dialog.show(activity?.supportFragmentManager!!, "PomodoroDialog")
         }
-        // 문제 원인) fragment 에서 observing 을 못하고 있다.
-        // 원인 예측) live data 를 setting 해주는 곳에서 쓰는 뷰모델 객체와 여기서 observing 하는 뷰모델 객체가 서로 다르다.
+        binding.rb25Minutes.setOnClickListener {
+            binding.tvPomodoroTimer.text = getString(R.string.tv_pomodoro_timer_25)
+        }
+        binding.rb50Minutes.setOnClickListener {
+            binding.tvPomodoroTimer.text = getString(R.string.tv_pomodoro_timer_50)
+        }
+        binding.btnStartPomodoro.setOnClickListener {
+            if(binding.tvSelectTodoItem.text == getString(R.string.tv_select_todo_item)) {
+                Toast.makeText(context, "할일을 선택해주세요!", Toast.LENGTH_SHORT).show()
+            } else {
+                when (binding.tvPomodoroTimer.text) {
+                    getString(R.string.tv_pomodoro_timer_25) -> {
+                        binding.rb25Minutes.isClickable = false
+                        binding.rb50Minutes.isClickable = false // [refactor] binding 도 많이 중복되니 scope function 으로 빼서 중복 피하기
+                        totalSecond = 5  // 25분 × 60초
+                        // worker thread
+                        timer = timer(
+                            initialDelay = 0,
+                            period = 1000
+                        ) { // period 단위 = millisecond ( 1000 millisecond = 1 second )
+                            if (totalSecond > 0) {
+                                totalSecond -= 1
+                                val currentHour = totalSecond / 3600
+                                val currentMinute = totalSecond / 60
+                                val currentSecond = totalSecond % 60
+                                requireActivity().runOnUiThread {
+                                    // worker thread 는 ui 에 접근할 수 없다. → Activity.runOnUiThread(Runnable) 이용
+                                    binding.tvPomodoroTimer.text =
+                                        String.format(
+                                            "%02d:%02d:%02d",
+                                            currentHour,
+                                            currentMinute,
+                                            currentSecond
+                                        )
+                                }
+                            } else {
+                                requireActivity().runOnUiThread {
+                                    Toast.makeText(context, "시간이 종료되었습니다!", Toast.LENGTH_SHORT)
+                                        .show()
+                                    binding.rb25Minutes.isChecked = false
+                                    binding.rb25Minutes.isClickable = true
+                                    binding.rb50Minutes.isClickable = true
+                                } // [?] 왜 25분을 다시 클릭했을 때 ui 색상이 안변하지?
+                                timer?.cancel()
+                                timer = null
+                            }
+                        }
+                    }
+                    getString(R.string.tv_pomodoro_timer_50) -> {
+                        binding.rb25Minutes.isClickable = false
+                        binding.rb50Minutes.isClickable = false
+                        totalSecond = 50 * 60
+                        timer = timer(initialDelay = 0, period = 1000) { // worker thread
+                            if (totalSecond > 0) { // [refactor] 반복되는 부분 함수로 빼기
+                                totalSecond -= 1
+                                val currentHour = totalSecond / 3600
+                                val currentMinute = totalSecond / 60
+                                val currentSecond = totalSecond % 60
+                                requireActivity().runOnUiThread {
+                                    binding.tvPomodoroTimer.text =
+                                        String.format(
+                                            "%02d:%02d:%02d",
+                                            currentHour,
+                                            currentMinute,
+                                            currentSecond
+                                        )
+                                }
+                            } else {
+                                requireActivity().runOnUiThread {
+                                    Toast.makeText(context, "시간이 종료되었습니다!", Toast.LENGTH_SHORT)
+                                        .show()
+                                    binding.rb50Minutes.isChecked = false
+                                    binding.rb25Minutes.isClickable = true
+                                    binding.rb50Minutes.isClickable = true
+                                }
+                                timer?.cancel()
+                                timer = null
+                            }
+                        }
+                    }
+                    else -> {
+                        Toast.makeText(context, "시간을 설정해주세요!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            }
+    }
+
+    fun observeViewModel() {
         sharedViewModel.selectedPosition.observe(viewLifecycleOwner) { selectedPosition ->
             binding.tvSelectTodoItem.text = Datas.todoItems[selectedPosition]
         }
     }
+
 
     companion object {
         /**
