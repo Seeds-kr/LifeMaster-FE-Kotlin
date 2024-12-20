0d0a082a6ce5d1efa13a3e922f085891dfe90987
SEYOUNG PARK <seyeongb18@gmail.com>
2024-12-20 17:05:49 +0900

    [feat] 디톡스 반복 잠금 다이얼로그 프래그먼트 연결

---
+++ app/src/main/java/com/example/lifemaster_xml/total/DetoxRepeatLockDialog.kt
@@ -0,0 +1,31 @@
+package com.example.lifemaster_xml.total
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import com.example.lifemaster_xml.R
+import com.example.lifemaster_xml.databinding.DialogDitoxRepeatLockBinding
+
+class DetoxRepeatLockDialog: DialogFragment(R.layout.dialog_ditox_repeat_lock) {
+
+    lateinit var binding: DialogDitoxRepeatLockBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DialogDitoxRepeatLockBinding.bind(view)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btnDelete.setOnClickListener {
+            dismiss()
+        }
+        binding.btnSave.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    companion object {
+        const val TAG = "DetoxRepeatLockDialog"
+    }
+}
