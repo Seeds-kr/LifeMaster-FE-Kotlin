4fb2459d88257c22d680814edbcce74c026edb18
SEYOUNG PARK <seyeongb18@gmail.com>
2024-12-30 19:00:55 +0900

    [design] 시간 잠금 설정 다이얼로그 - TextInputLayout - AutoCompleteTextView - style 지정 가능 - item_dropdown.xml → 상위 레이아웃 없이 TextView 만 사용 가능

--- app/src/main/java/com/example/lifemaster_xml/total/detox/dialog/DetoxTimeLockDialog.kt
+++ app/src/main/java/com/example/lifemaster_xml/total/detox/dialog/DetoxTimeLockDialog.kt
@@ -2,6 +2,7 @@ package com.example.lifemaster_xml.total.detox.dialog
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.DialogFragment
 import com.example.lifemaster_xml.R
 import com.example.lifemaster_xml.databinding.DialogDetoxTimeLockBinding
@@ -13,6 +14,17 @@ class DetoxTimeLockDialog: DialogFragment(R.layout.dialog_detox_time_lock) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DialogDetoxTimeLockBinding.bind(view)
+
+        // 드롭다운 - 반복 기간
+        val periodArray = resources.getStringArray(R.array.detox_time_lock_repeat_period)
+        val periodAdapter = ArrayAdapter(requireContext(), R.layout.item_dropdown, periodArray)
+        binding.autoCompleteTextViewPeriod.setAdapter(periodAdapter)
+
+        // 드롭다운 - 반복 요일
+        val dayArray = resources.getStringArray(R.array.detox_time_lock_repeat_day)
+        val dayAdapter = ArrayAdapter(requireContext(), R.layout.item_dropdown, dayArray)
+        binding.autoCompleteTextViewDay.setAdapter(dayAdapter)
+
         setupListeners()
     }
 
