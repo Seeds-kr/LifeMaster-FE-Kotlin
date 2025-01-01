0af96fe94485f1e3962589e296a73893c5c0a8d2
SEYOUNG PARK <seyeongb18@gmail.com>
2025-01-01 14:28:28 +0900

    [design] 시간 잠금 설정 다이얼로그 - time picker 사용 - 시작 시간과 종료 시간 구분 - 기존 EditText 에서 Button 으로 변경

--- app/src/main/java/com/example/lifemaster_xml/total/detox/dialog/DetoxTimeLockDialog.kt
+++ app/src/main/java/com/example/lifemaster_xml/total/detox/dialog/DetoxTimeLockDialog.kt
@@ -1,5 +1,6 @@
 package com.example.lifemaster_xml.total.detox.dialog
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
@@ -29,12 +30,56 @@ class DetoxTimeLockDialog: DialogFragment(R.layout.dialog_detox_time_lock) {
     }
 
     private fun setupListeners() {
-        binding.btnAdd.setOnClickListener {
-            dismiss()
+
+        // 시작 시간
+        binding.btnStartHour.setOnClickListener {
+            showTimePickerDialogAndSetStartTime()
         }
+        binding.btnStartMinutes.setOnClickListener {
+            showTimePickerDialogAndSetStartTime()
+        }
+
+        // 종료 시간
+        binding.btnEndHour.setOnClickListener {
+            showTimePickerDialogAndSetEndTime()
+        }
+        binding.btnEndMinutes.setOnClickListener {
+            showTimePickerDialogAndSetEndTime()
+        }
+
         binding.btnCancel.setOnClickListener {
             dismiss()
         }
+    }
+
+    private fun showTimePickerDialogAndSetStartTime() {
+        val timePickerDialog = TimePickerDialog(
+            context,
+            { _, hour, minutes ->
+                binding.btnStartHour.text = if(hour in 0..12) hour.toString() else (hour-12).toString()
+                binding.btnStartMinutes.text = minutes.toString()
+                binding.btnStartDayPart.text = if(hour in 0..11) "AM" else "PM"
+            },
+            10,
+            20,
+            false
+        )
+        timePickerDialog.show()
+    }
+
+    private fun showTimePickerDialogAndSetEndTime() {
+        val timePickerDialog = TimePickerDialog(
+            context,
+            { _, hour, minutes ->
+                binding.btnEndHour.text = if(hour in 0..12) hour.toString() else (hour-12).toString()
+                binding.btnEndMinutes.text = minutes.toString()
+                binding.btnEndDayPart.text = if(hour in 0..11) "AM" else "PM"
+            },
+            10,
+            20,
+            false
+        )
+        timePickerDialog.show()
     }
 
     companion object {
