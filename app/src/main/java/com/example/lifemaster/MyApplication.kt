51f3093fdd7e7af3c0b1d1cdbd3c7984e782083b
SEYOUNG PARK <seyeongb18@gmail.com>
2025-01-03 18:37:47 +0900

    [feat] 카카오 로그인 API 구현하기 - authentication 용 activity 한개 더 생성 (bottom navigation view 로 인해 fragment 단위로 할 수 없다고 판단)

--- app/src/main/java/com/example/lifemaster/MyApplication.kt
+++ app/src/main/java/com/example/lifemaster/MyApplication.kt
@@ -2,12 +2,12 @@ package com.example.lifemaster
 
 import android.app.Application
 import android.util.Log
+import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        val keyHash = Utility.getKeyHash(this)
-        Log.d("keyHash", keyHash)
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
     }
 }
---
+++ app/src/main/java/com/example/lifemaster/authentication/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.example.lifemaster.authentication
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lifemaster.MainActivity
+import com.example.lifemaster.R
+import com.example.lifemaster.databinding.ActivityLoginBinding
+import com.kakao.sdk.user.UserApiClient
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btnKakaoLogin.setOnClickListener {
+            loginWithKaKao(
+                context = this,
+                onSuccess = { token ->
+                    Log.d("kakao login token : ", token)
+                    Toast.makeText(this, "로그인에 성공하였습니다!", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                },
+                onFailure = { error ->
+                    Log.d("kakao login error : ", error.toString())
+                    Toast.makeText(this, "로그인에 실패하였습니다!", Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
+    }
+
+    fun loginWithKaKao(
+        context: Context,
+        onSuccess: (String) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ) {
+        UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+            if(error != null) {
+                onFailure(error)
+            } else if(token != null) {
+                onSuccess(token.accessToken)
+            }
+        }
+    }
+}
