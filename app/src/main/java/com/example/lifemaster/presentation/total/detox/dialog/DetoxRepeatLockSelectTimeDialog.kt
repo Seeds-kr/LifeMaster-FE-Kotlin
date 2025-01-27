a6c72e5c5292a63bc996cde2ed2578e033e3e5cc
SEYOUNG PARK <seyeongb18@gmail.com>
2025-01-27 17:30:18 +0900

    [feat] 디톡스 반복잠금 시간 단위(분) 수정 - TimePicker 기능 수정 - 기존: 0~59 분 사이를 스크롤 가능하게 함 - UX 관점에서 사용자가 불편함을 느낄 수 있다고 생각 - 기존 1분 단위에서 5분 단위로 변경

--- app/src/main/java/com/example/lifemaster/presentation/total/detox/dialog/DetoxRepeatLockSelectTimeDialog.kt
+++ app/src/main/java/com/example/lifemaster/presentation/total/detox/dialog/DetoxRepeatLockSelectTimeDialog.kt
@@ -1,6 +1,7 @@
 package com.example.lifemaster.presentation.total.detox.dialog
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
@@ -8,7 +9,8 @@ import com.example.lifemaster.R
 import com.example.lifemaster.databinding.DialogSelectTimesBinding
 import com.example.lifemaster.presentation.total.detox.viewmodel.DetoxRepeatLockViewModel
 
-class DetoxRepeatLockSelectTimeDialog(private val type: String) : DialogFragment(R.layout.dialog_select_times) {
+class DetoxRepeatLockSelectTimeDialog(private val type: String) :
+    DialogFragment(R.layout.dialog_select_times) {
     private lateinit var binding: DialogSelectTimesBinding
     private val viewModel: DetoxRepeatLockViewModel by activityViewModels()
 
@@ -20,30 +22,35 @@ class DetoxRepeatLockSelectTimeDialog(private val type: String) : DialogFragment
     }
 
     private fun setupViews() {
+        val values = Array(12) { (it * 5).toString() }
         binding.npHour.minValue = 0
         binding.npHour.maxValue = 12
-        binding.npMinutes.minValue = 0
-        binding.npMinutes.maxValue = 59
-        when(type) {
+        binding.npMinutes.minValue = 0 // 배열의 첫번째 인덱스
+        binding.npMinutes.maxValue = 11 // 배열의 마지막 인덱스
+        binding.npMinutes.displayedValues = values
+        when (type) {
             "useTime" -> {
                 binding.npHour.value = viewModel.useTime.value?.first ?: 0
-                binding.npMinutes.value = viewModel.useTime.value?.second ?:0
+                binding.npMinutes.value = (viewModel.useTime.value?.second?.div(5)) ?: 0
             }
+
             "lockTime" -> {
                 binding.npHour.value = viewModel.lockTime.value?.first ?: 0
-                binding.npMinutes.value = viewModel.lockTime.value?.second ?: 0
+                binding.npMinutes.value = viewModel.lockTime.value?.second?.div(5) ?: 0
             }
+
             "maxUseTime" -> {
                 binding.npHour.value = viewModel.maxUseTime.value?.first ?: 0
-                binding.npMinutes.value = viewModel.maxUseTime.value?.second ?: 0
+                binding.npMinutes.value = viewModel.maxUseTime.value?.second?.div(5) ?: 0
             }
         }
     }
 
     private fun setupListeners() {
         binding.btnSelect.setOnClickListener {
-            val time = Pair(binding.npHour.value, binding.npMinutes.value)
-            when(type) {
+            Log.d("ttest", ""+binding.npMinutes.value)
+            val time = Pair(binding.npHour.value, binding.npMinutes.value*5)
+            when (type) {
                 "useTime" -> viewModel.setUseTime(time)
                 "lockTime" -> viewModel.setLockTime(time)
                 "maxUseTime" -> viewModel.setMaxUseTime(time)
