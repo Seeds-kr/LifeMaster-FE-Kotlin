d07f2db6afaa049910e717c47aa1e33af301aa8b
SEYOUNG PARK <seyeongb18@gmail.com>
2025-03-10 20:42:24 +0900

    [refactor] 할일 어댑터 리팩토링 - recyclerview adapter 에서 list adapter 로 변경 - diffUtil 사용하여 성능 개선 - 포모도로 관련한 부분은 추후 수정하기 위해 주석 처리

--- app/src/main/java/com/example/lifemaster/presentation/home/todo/ToDoAdapter.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/todo/ToDoAdapter.kt
@@ -7,42 +7,51 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lifemaster.R
-import com.example.lifemaster.presentation.total.pomodoro.PomodoroFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.lifemaster.model.TodoItem
+import androidx.recyclerview.widget.ListAdapter
+import com.example.lifemaster.databinding.ItemTodoBinding
 
-class ToDoAdapter(
-    val todoItems: MutableList<String>,
-    val context: Context,
-    val activity: FragmentActivity // 단일 액티비티 사용
-) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() {
-    inner class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val content: TextView
-        val goToPomodoro: ImageView
-        init {
-            content = itemView.findViewById(R.id.tv_todo_item)
-            goToPomodoro = itemView.findViewById(R.id.iv_go_to_pomodoro)
+class ToDoAdapter: ListAdapter<TodoItem, ToDoAdapter.ToDoViewHolder>(differ) {
+    inner class ToDoViewHolder(private val binding: ItemTodoBinding): RecyclerView.ViewHolder(binding.root) {
+//        val content: TextView
+//        val goToPomodoro: ImageView
+//        init {
+//            content = itemView.findViewById(R.id.tv_todo_item)
+//            goToPomodoro = itemView.findViewById(R.id.iv_go_to_pomodoro)
+//
+//            goToPomodoro.setOnClickListener {
+////                // 1. home tab -> total view 전환
+////                activity.findViewById<BottomNavigationView>(R.id.navigation).selectedItemId = R.id.action_total
+////                // 2. total view 중 포모도로 프래그먼트 화면에 보이기
+////                activity.supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, PomodoroFragment(), "PomodoroFragment").commit()
+//            }
+//        }
 
-            goToPomodoro.setOnClickListener {
-                // 1. home tab -> total view 전환
-                activity.findViewById<BottomNavigationView>(R.id.navigation).selectedItemId = R.id.action_total
-                // 2. total view 중 포모도로 프래그먼트 화면에 보이기
-                activity.supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, PomodoroFragment(), "PomodoroFragment").commit()
-            }
+        fun bind(item: TodoItem) = with(binding) {
+            tvTitle.text = item.title
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-        val itemView = LayoutInflater.from(context).inflate(R.layout.item_todo, parent, false) // [!] 무조건 false 를 전달해야한다.
-        return ToDoViewHolder(itemView)
+        return ToDoViewHolder(ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
-        holder.content.text = todoItems[position]
+        holder.bind(currentList[position])
     }
 
-    override fun getItemCount(): Int {
-        return todoItems.size
+    companion object {
+        val differ = object: DiffUtil.ItemCallback<TodoItem>() {
+            override fun areContentsTheSame(oldItem: TodoItem, newItem: TodoItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areItemsTheSame(oldItem: TodoItem, newItem: TodoItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
     }
 }
