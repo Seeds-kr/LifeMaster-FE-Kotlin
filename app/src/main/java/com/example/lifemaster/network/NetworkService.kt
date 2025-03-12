515f479564a6be9fd9f78527d0c7a74479f38c8b
SEYOUNG PARK <seyeongb18@gmail.com>
2025-03-12 22:03:53 +0900

    [feat] 할일 백엔드 메소드 추가 - 특정 항목 조회, 업데이트, 토글 변경 메소드 추가 - 주석 추가하여 메소드 역할 가시화 - 나중에 연결하기 편하도록 미리 메소드 정의

--- app/src/main/java/com/example/lifemaster/network/NetworkService.kt
+++ app/src/main/java/com/example/lifemaster/network/NetworkService.kt
@@ -4,22 +4,48 @@ import com.example.lifemaster.model.TodoItem
 import retrofit2.Call
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NetworkService {
+
+    // 모든 To-Do 항목 조회
     @GET("/schedule/todo")
     fun getTodoItems(): Call<List<TodoItem>>
 
+    // 새 To-Do 생성
     @POST("/schedule/todo/create")
     fun registerTodoItem(
         @Query("date") date: String,
         @Query("title") title: String
     ): Call<TodoItem>
 
+    // To-Do 삭제
     @DELETE("/schedule/todo/{id}")
     fun deleteTodoItem(
         @Path("id") id: Int
     ):Call<Any>
+
+    // 특정 To-Do 조회
+    @GET("/schedule/todo/{id}")
+    fun getTodoItem(
+        @Path("id") id: Int
+    ):Call<TodoItem>
+
+    // To-Do 업데이트
+    @PUT("/schedule/todo/{id}")
+    fun updateTodoItem(
+        @Query("id") id: Int,
+        @Query("date") date: String,
+        @Query("title") title: String
+    ):Call<TodoItem>
+
+    // To-Do 완료 상태 토글
+    @PATCH("/schedule/todo/{id}/toggle-completed")
+    fun toggleTodoItem(
+        @Path("id") id: Int
+    ):Call<TodoItem>
 }
