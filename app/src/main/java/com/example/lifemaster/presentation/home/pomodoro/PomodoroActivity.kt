36ac13a469a170c9af062b972d9cf960f7ac9856
SEYOUNG PARK <seyeongb18@gmail.com>
2025-03-19 21:13:03 +0900

    [fix] 포모도로 타이머 시간 단위 변경 - hour, minute, second 단위 (기존) - minute, second, decisecond 단위 (이후)

--- app/src/main/java/com/example/lifemaster/presentation/home/pomodoro/PomodoroActivity.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/pomodoro/PomodoroActivity.kt
@@ -2,6 +2,7 @@ package com.example.lifemaster.presentation.home.pomodoro
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lifemaster.R
@@ -16,7 +17,7 @@ class PomodoroActivity : AppCompatActivity() {
 
 //    private val sharedViewModel: PomodoroViewModel by viewModels()
 
-    private var totalSecond = 0
+    private var totalDeciSecond = 0
 
     private var timer: Timer? = null
 
@@ -35,93 +36,90 @@ class PomodoroActivity : AppCompatActivity() {
     }
 
     private fun initListeners() = with(binding) {
-//        binding.ivOpenTodoDialog.setOnClickListener {
-//            val dialog = PomodoroDialog()
-//            dialog.isCancelable = false
-//            dialog.show(supportFragmentManager, PomodoroDialog.TAG)
-//        }
         rb25Minutes.setOnClickListener {
-            tvPomodoroTimer.text = getString(R.string.tv_pomodoro_timer_25)
+            tvMinutesAndSeconds.text = getString(R.string.tv_pomodoro_timer_25)
         }
         rb50Minutes.setOnClickListener {
-            tvPomodoroTimer.text = getString(R.string.tv_pomodoro_timer_50)
+            tvMinutesAndSeconds.text = getString(R.string.tv_pomodoro_timer_50)
         }
         btnStartPomodoro.setOnClickListener {
-            if(tvPomodoroTimer.text.toString() == getString(R.string.tv_pomodoro_timer_25) && tvTodoItemTitle.text.toString()!=getString(R.string.item_is_not_selected)) {
-                rb25Minutes.isClickable = false
-                rb50Minutes.isClickable = false // [refactor] binding 도 많이 중복되니 scope function 으로 빼서 중복 피하기
+            if (tvMinutesAndSeconds.text.toString() == getString(R.string.tv_pomodoro_timer_25)) {
+                rgTimer.visibility = View.INVISIBLE
 //                sharedViewModel.clickButton()
-                totalSecond = 25*60  // 25분 × 60초
-//                totalSecond = 5 // test 용
+                totalDeciSecond = 25 * 60 * 10  // 25분 × 60초
                 // worker thread
                 timer = timer(
                     initialDelay = 0,
-                    period = 1000
-                ) { // period 단위 = millisecond ( 1000 millisecond = 1 second )
-                    if (totalSecond > 0) {
-                        totalSecond -= 1
-                        val currentHour = totalSecond / 3600
-                        val currentMinute = totalSecond / 60
-                        val currentSecond = totalSecond % 60
+                    period = 100 // period 단위 = millisecond ( 100 millisecond = 0.1 second = 1 decisecond )
+                ) {
+                    if (totalDeciSecond > 0) {
+                        totalDeciSecond -= 1
+                        val minutes = totalDeciSecond.div(10) / 60
+                        val seconds = totalDeciSecond.div(10) % 60
+                        val deciseconds = totalDeciSecond % 10
                         runOnUiThread {
                             // worker thread 는 ui 에 접근할 수 없다. → Activity.runOnUiThread(Runnable) 이용
-                            tvPomodoroTimer.text =
+                            tvMinutesAndSeconds.text =
                                 String.format(
-                                    "%02d:%02d:%02d",
-                                    currentHour,
-                                    currentMinute,
-                                    currentSecond
+                                    "%02d:%02d",
+                                    minutes,
+                                    seconds
                                 )
+                            tvMillisecond.text = deciseconds.toString()
                         }
                     } else {
                         runOnUiThread {
-                            Toast.makeText(this@PomodoroActivity, "시간이 종료되었습니다!", Toast.LENGTH_SHORT).show()
-                            rb25Minutes.isChecked = false
-                            rb25Minutes.isClickable = true
-                            rb50Minutes.isClickable = true
-//                            sharedViewModel.resetButtonCount()
-                        } // [fix] 왜 25분을 다시 클릭했을 때 ui 색상이 안변하지?
+                            Toast.makeText(
+                                this@PomodoroActivity,
+                                "시간이 종료되었습니다!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // PomodoroActivity → MainActivity 의 Home Fragment 로 이동
+                            // 해당 아이템의 할일 체크 상태 변경 알리기
+                            // sharedViewModel.resetButtonCount()
+                        }
                         timer?.cancel()
                         timer = null
-                        // totalSecond = 0
                     }
                 }
-            } else if(tvPomodoroTimer.text.toString() == getString(R.string.tv_pomodoro_timer_50) && binding.tvTodoItemTitle.text.toString()!=getString(R.string.item_is_not_selected)) {
-                rb25Minutes.isClickable = false
-                rb50Minutes.isClickable = false
+            }
+            else if (tvMinutesAndSeconds.text.toString() == getString(R.string.tv_pomodoro_timer_50)) {
 //                sharedViewModel.clickButton()
-                totalSecond = 50 * 60
-                timer = timer(initialDelay = 0, period = 1000) { // worker thread
-                    if (totalSecond > 0) { // [refactor] 반복되는 부분 함수로 빼기
-                        totalSecond -= 1
-                        val currentHour = totalSecond / 3600
-                        val currentMinute = totalSecond / 60
-                        val currentSecond = totalSecond % 60
+                rgTimer.visibility = View.INVISIBLE
+                totalDeciSecond = 50 * 60 * 10
+                timer = timer(initialDelay = 0, period = 100) { // worker thread
+                    if (totalDeciSecond > 0) {
+                        totalDeciSecond -= 1
+                        val minutes = totalDeciSecond.div(10) / 60
+                        val seconds = totalDeciSecond.div(10) % 60
+                        val deciseconds = totalDeciSecond % 10
                         runOnUiThread {
-                            tvPomodoroTimer.text =
+                            tvMinutesAndSeconds.text =
                                 String.format(
-                                    "%02d:%02d:%02d",
-                                    currentHour,
-                                    currentMinute,
-                                    currentSecond
+                                    "%02d:%02d",
+                                    minutes,
+                                    seconds
                                 )
+                            tvMillisecond.text = deciseconds.toString()
                         }
                     } else {
                         runOnUiThread {
-                            Toast.makeText(this@PomodoroActivity, "시간이 종료되었습니다!", Toast.LENGTH_SHORT)
-                                .show()
-                            rb50Minutes.isChecked = false
-                            rb25Minutes.isClickable = true
-                            rb50Minutes.isClickable = true
+                            Toast.makeText(
+                                this@PomodoroActivity,
+                                "시간이 종료되었습니다!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // PomodoroActivity → MainActivity 의 Home Fragment 로 이동
+                            // 해당 아이템의 할일 체크 상태 변경 알리기
 //                            sharedViewModel.resetButtonCount()
                         }
                         timer?.cancel()
                         timer = null
                     }
                 }
-            } else if(tvTodoItemTitle.text == getString(R.string.item_is_not_selected) || tvPomodoroTimer.text == getString(R.string.tv_pomodoro_timer_not_set)) {
-                Toast.makeText(this@PomodoroActivity, "아직 설정되지 않은 값이 있습니다!", Toast.LENGTH_SHORT).show()
-            }  else {
+            } else if (tvMinutesAndSeconds.text == getString(R.string.tv_pomodoro_timer_not_set)) {
+                Toast.makeText(this@PomodoroActivity, "시간을 설정해주세요!", Toast.LENGTH_SHORT).show()
+            } else {
 //                sharedViewModel.clickButton()
             }
         }
@@ -150,38 +148,38 @@ class PomodoroActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        if(SharedData.pomodoroStatus == PomodoroStatus.ESCAPE_SUCCESS) {
+        if (SharedData.pomodoroStatus == PomodoroStatus.ESCAPE_SUCCESS) {
             // 해당 할 일을 아직 다 끝내지 못한 상태
 //            sharedViewModel.resetButtonCount()
             timer?.cancel()
             timer = null
-            totalSecond = 0
-            binding.tvPomodoroTimer.text =
-                getString(R.string.tv_pomodoro_timer_not_set) // [?] zero? not set?
+            totalDeciSecond = 0
+//            binding.tvPomodoroTimer.text =
+//                getString(R.string.tv_pomodoro_timer_not_set) // [?] zero? not set?
             binding.rb25Minutes.isChecked = false
             binding.rb50Minutes.isChecked = false
             binding.rb25Minutes.isClickable = true
             binding.rb50Minutes.isClickable = true
             Toast.makeText(this, "비상탈출을 완료했습니다!", Toast.LENGTH_SHORT).show()
-        } else if(SharedData.pomodoroStatus == PomodoroStatus.ESCAPE_FAILURE) {
+        } else if (SharedData.pomodoroStatus == PomodoroStatus.ESCAPE_FAILURE) {
             timer = timer(
                 initialDelay = 1000,
                 period = 1000
             ) { // period 단위 = millisecond ( 1000 millisecond = 1 second )
-                if (totalSecond > 0) {
-                    totalSecond -= 1
-                    val currentHour = totalSecond / 3600
-                    val currentMinute = totalSecond / 60
-                    val currentSecond = totalSecond % 60
+                if (totalDeciSecond > 0) {
+                    totalDeciSecond -= 1
+                    val currentHour = totalDeciSecond / 3600
+                    val currentMinute = totalDeciSecond / 60
+                    val currentSecond = totalDeciSecond % 60
                     runOnUiThread {
                         // worker thread 는 ui 에 접근할 수 없다. → Activity.runOnUiThread(Runnable) 이용
-                        binding.tvPomodoroTimer.text =
-                            String.format(
-                                "%02d:%02d:%02d",
-                                currentHour,
-                                currentMinute,
-                                currentSecond
-                            )
+//                        binding.tvPomodoroTimer.text =
+//                            String.format(
+//                                "%02d:%02d:%02d",
+//                                currentHour,
+//                                currentMinute,
+//                                currentSecond
+//                            )
                     }
                 } else {
                     runOnUiThread {
