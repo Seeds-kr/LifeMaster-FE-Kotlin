deca9d21e2c068d45a3569a6e0fe0a4c59d97ea3
se05503 <seyeongb18@gmail.com>
2025-04-24 23:37:12 +0900

    test(alarm): 알람 기능 동작 확인 테스트 2 [시도한 부분] - 기존 액티비티 곧바로 실행 → 알림 권한 & 알림 발생 후 액티비티 이동 - 액티비티에서 MediaPlayer 사용하여 알람음 울림 & 뒤로가기 방지 - 알람 울림 화면 디자인 변경 [막힌 점/아쉬운점] - 액티비티에서 아예 앱을 종료할 경우에도 알람음을 유지하는 방법? - 알림이 발생했을 때 화면을 터치하면 소리가 꺼짐 [다음에 시도할 부분] - Notification 에서는 소리 설정x - MediaPlayer 을 ForegroundService 에서 만들어서 서비스 실행하기

--- app/src/main/java/com/example/lifemaster/presentation/group/view/activity/AlarmRingsActivity.kt
+++ app/src/main/java/com/example/lifemaster/presentation/group/view/activity/AlarmRingsActivity.kt
@@ -1,26 +1,68 @@
 package com.example.lifemaster.presentation.group.view.activity
 
+import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.lifemaster.R
 import com.example.lifemaster.databinding.ActivityAlarmRingsBinding
+import com.example.lifemaster.presentation.home.pomodoro.PomodoroActivity
 
 class AlarmRingsActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityAlarmRingsBinding
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityAlarmRingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 알람 소리 시작
+        mediaPlayer = MediaPlayer.create(this, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM))
+        mediaPlayer?.isLooping = true
+        mediaPlayer?.start()
+        initListeners()
     }
+
+    private fun initListeners() = with(binding) {
+        tvStopAlarm.setOnClickListener {
+            mediaPlayer?.stop()
+            mediaPlayer?.release()
+            mediaPlayer = null
+            Toast.makeText(this@AlarmRingsActivity, "알람이 종료되었습니다!", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        // 뒤로가기 버튼(백버튼) 막기
+        onBackPressedDispatcher.addCallback(
+            this@AlarmRingsActivity,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    Toast.makeText(this@AlarmRingsActivity, "뒤로 가기로 앱을 종료할 수 없습니다!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ttest", "onDestroy")
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+
 }
