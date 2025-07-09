2074eb1ba8f9913caf7c1b58314180aad35d593b
se05503 <seyeongb18@gmail.com>
2025-07-09 13:20:50 +0900

    [chore] 자잘한 수정, 파일 커밋

--- app/src/main/java/com/example/lifemaster/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/lifemaster/network/RetrofitInstance.kt
@@ -5,7 +5,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://ec2-52-79-242-42.ap-northeast-2.compute.amazonaws.com:8080"
+
+    private const val BASE_URL = ""
+
     val networkService: NetworkService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
