48b4b648e32b1fa5c4a16086297c508ec461b50d
hyewon <hyewon030831@naver.com>
2025-07-18 09:25:06 +0900

    [feat] 주석 수정

--- app/src/main/java/com/example/lifemaster/presentation/login/view/ResetPasswordFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/login/view/ResetPasswordFragment.kt
@@ -49,7 +49,7 @@ class ResetPasswordFragment : Fragment(R.layout.fragment_reset_password) {
             // 임시 성공 처리
             Toast.makeText(requireContext(), "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show()
 
-            // TODO: API 연동 후 로그인 화면으로 이동
+            // API 연동 후 로그인 화면으로 이동
             // findNavController().navigate(R.id.loginEmailFragment)
         }
     }
