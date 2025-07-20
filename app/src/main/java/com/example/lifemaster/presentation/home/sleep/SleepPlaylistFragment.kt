7f3905fca60d18311c8c757f642def3f0d745194
se05503 <seyeongb18@gmail.com>
2025-07-20 19:59:44 +0900

    [feat] 수면 플레이리스트(백색소음) 더보기/접기 기능 구현 1. 방식 변경 - 기존 recyclerview → 부모 뷰(LinearLayout)에 addview 로 자식 뷰 추가 - NestedScrollView → ScrollView 변경 2. 터치 이벤트 영역 확장 - 아이콘이 받는 터치 이벤트 영역이 너무 작음 → 더보기와 아이콘을 상위 레이아웃으로 한번 더 감싸서 영역 확장 3. 샘플 데이터 일부 제거 - 테스트 용이성

--- app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepPlaylistFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepPlaylistFragment.kt
@@ -1,16 +1,17 @@
 package com.example.lifemaster.presentation.home.sleep
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.lifemaster.R
 import com.example.lifemaster.databinding.FragmentSleepPlaylistBinding
+import com.example.lifemaster.databinding.ItemSleepPlaylistBinding
 
 
 class SleepPlaylistFragment : Fragment(R.layout.fragment_sleep_playlist) {
 
     lateinit var binding: FragmentSleepPlaylistBinding
-    private val sleepPlaylistAdapter = SleepPlaylistAdapter()
     private val sampleMusic = listOf(
         SleepItem(
             id = 1,
@@ -32,27 +33,61 @@ class SleepPlaylistFragment : Fragment(R.layout.fragment_sleep_playlist) {
             duration = "02:00:00",
             thumbnail = R.drawable.tmp_sleep_playlist,
             description = "포브스 선정 잘 때 틀면 꿀잠 자는 백색소음 Top 100 중에 102위"
-        ),
-        SleepItem(
-            id = 4,
-            title = "꿀잠 자는 백색소음 4",
-            duration = "01:30:45",
-            thumbnail = R.drawable.tmp_sleep_playlist,
-            description = "포브스 선정 잘 때 틀면 꿀잠 자는 백색소음 Top 100 중에 103위"
-        ),
-        SleepItem(
-            id = 5,
-            title = "꿀잠 자는 백색소음 5",
-            duration = "00:55:10",
-            thumbnail = R.drawable.tmp_sleep_playlist,
-            description = "포브스 선정 잘 때 틀면 꿀잠 자는 백색소음 Top 100 중에 104위"
         )
     )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSleepPlaylistBinding.bind(view)
-        binding.recyclerviewSleepPlaylistWhiteNoise.adapter = sleepPlaylistAdapter
-        sleepPlaylistAdapter.submitList(sampleMusic)
+        initViews()
+        initListeners()
+
+    }
+
+    private fun initListeners() = with(binding) {
+        llSleepPlaylistWhiteNoiseViewMore.setOnClickListener {
+
+            if (tvSleepPlaylistWhiteNoiseViewMore.text == resources.getString(R.string.sleep_playlist_view_more)) {
+                // step 1. 뷰 상태 변경
+                tvSleepPlaylistWhiteNoiseViewMore.text = resources.getString(R.string.sleep_playlist_view_less)
+                ivSleepPlaylistWhiteNoiseViewMore.rotation = 180f // TODO: rotation, rotationX, rotationY 차이 이해하기
+                // step 2. 뷰 추가
+                for (i in 2 until sampleMusic.size) {
+                    val playlistView = ItemSleepPlaylistBinding.inflate(
+                        LayoutInflater.from(context),
+                        llSleepPlaylistWhiteNoise,
+                        false
+                    )
+                    playlistView.ivSleepPlaylistItemThumbnail.setImageResource(sampleMusic[i].thumbnail)
+                    playlistView.tvSleepPlaylistItemTitle.text = sampleMusic[i].title
+                    playlistView.tvSleepPlaylistItemDuration.text = sampleMusic[i].duration
+                    playlistView.tvSleepPlaylistItemDescription.text = sampleMusic[i].description
+                    llSleepPlaylistWhiteNoise.addView(playlistView.root)
+                }
+            } else {
+                // step 1. 뷰 상태 변경
+                tvSleepPlaylistWhiteNoiseViewMore.text = resources.getString(R.string.sleep_playlist_view_more)
+                ivSleepPlaylistWhiteNoiseViewMore.rotation = 180f
+                // step 2. 뷰 제거
+                llSleepPlaylistWhiteNoise.removeViews(2, sampleMusic.size-2)
+            }
+        }
+    }
+
+    private fun initViews() = with(binding) {
+
+        // 2개만 추가 (i = 0 → 1)
+        for (i in 0..1) {
+            val playlistView = ItemSleepPlaylistBinding.inflate(
+                LayoutInflater.from(context),
+                llSleepPlaylistWhiteNoise,
+                false
+            )
+            playlistView.ivSleepPlaylistItemThumbnail.setImageResource(sampleMusic[i].thumbnail)
+            playlistView.tvSleepPlaylistItemTitle.text = sampleMusic[i].title
+            playlistView.tvSleepPlaylistItemDuration.text = sampleMusic[i].duration
+            playlistView.tvSleepPlaylistItemDescription.text = sampleMusic[i].description
+            llSleepPlaylistWhiteNoise.addView(playlistView.root)
+        }
     }
 }
