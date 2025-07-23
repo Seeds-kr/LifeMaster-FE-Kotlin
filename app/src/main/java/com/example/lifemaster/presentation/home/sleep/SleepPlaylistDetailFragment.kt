d3212b097440448ea2062b104af83f6e0c8ffe88
se05503 <seyeongb18@gmail.com>
2025-07-23 21:02:24 +0900

    [test] 수면 음악 프로그래스바 진행도 표시하기 테스트 - 재생 버튼을 눌렀을 때 노래 길이 만큼 프로그래스바가 증가함 - handler 사용 - 노래 재생 완료 시 mediaPlayer 메모리 해제 - 단위: 밀리초(ms) - 중지 버튼 클릭 이벤트 동작은 아직 반영 x

--- app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepPlaylistDetailFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepPlaylistDetailFragment.kt
@@ -2,6 +2,8 @@ package com.example.lifemaster.presentation.home.sleep
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -13,6 +15,7 @@ class SleepPlaylistDetailFragment : Fragment(R.layout.fragment_sleep_playlist_de
     private lateinit var binding: FragmentSleepPlaylistDetailBinding
     private var mediaPlayer: MediaPlayer ?= null
     private var isPlaying: Boolean = false
+    private var songDuration: Int = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,13 +28,35 @@ class SleepPlaylistDetailFragment : Fragment(R.layout.fragment_sleep_playlist_de
         ivSleepMainPlayToggle.setOnClickListener {
             if(mediaPlayer == null) {
                 // 맨 처음 재생
-                mediaPlayer = MediaPlayer.create(context, R.raw.sleep_classic_gymnopedie_no1)
+                mediaPlayer = MediaPlayer.create(context, R.raw.sleep_classic_gymnopedie_no1).apply {
+                    songDuration = duration
+                    progressSleepMain.max = duration
+                }
                 mediaPlayer?.start()
                 isPlaying = true
                 ivSleepMainPlayToggle.setImageResource(R.drawable.ic_pause)
+                // 프로그래스바 UI 변경
+                val handler = Handler(Looper.getMainLooper())
+                val songStartTime = System.currentTimeMillis() // 노래 처음 시작 시간 (단위: ms) → 정적
+                val updateProgressBarTask = object: Runnable {
+                    override fun run() {
+                        val songElapsedTime = System.currentTimeMillis() - songStartTime // 노래 누적 재생 시간 (단위: ms) → 동적
+                        if(songElapsedTime <= songDuration) {
+                            progressSleepMain.setProgress(songElapsedTime.toInt(), true)
+                            handler.postDelayed(this, 100L) // 100ms 마다 실행
+                        } else {
+                            // 초기화
+                            mediaPlayer?.stop()
+                            mediaPlayer?.release()
+                            mediaPlayer = null
+                            progressSleepMain.progress = 0
+                            ivSleepMainPlayToggle.setImageResource(R.drawable.ic_play_no_background)
+                        }
+                    }
+                }
+                handler.post(updateProgressBarTask)
             } else {
                 if(isPlaying) {
-                    // 멈추기
                     mediaPlayer?.pause()
                     isPlaying = false
                     ivSleepMainPlayToggle.setImageResource(R.drawable.ic_play_no_background)
