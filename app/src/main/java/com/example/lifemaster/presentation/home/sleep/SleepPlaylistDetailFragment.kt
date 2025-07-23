4aa9ef46ab161425b7e4ba33388cff825bd1810b
se05503 <seyeongb18@gmail.com>
2025-07-23 16:04:51 +0900

    [test] 수면 음악 재생, 멈춤 기능 테스트하기 - 위치: SleepPlaylistDetailFragment - 클래식 음악) 에릭사티 '짐노페디' 1번 mp3 음원 추출 - mp3 주의할점 → 네이밍 컨벤션 안맞으면 소스 못찾아옴 (대문자, _ 외의 특수문자 사용) - 재생, 멈춤 기능 구현 - 2가지 재생 아이콘에 대해 네이밍 변경

--- app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepPlaylistDetailFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepPlaylistDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.lifemaster.presentation.home.sleep
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -10,12 +11,36 @@ import com.example.lifemaster.databinding.FragmentSleepPlaylistDetailBinding
 class SleepPlaylistDetailFragment : Fragment(R.layout.fragment_sleep_playlist_detail) {
 
     private lateinit var binding: FragmentSleepPlaylistDetailBinding
+    private var mediaPlayer: MediaPlayer ?= null
+    private var isPlaying: Boolean = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSleepPlaylistDetailBinding.bind(view)
-        binding.llSleepMainPlaylist.setOnClickListener {
-            findNavController().navigate(R.id.action_sleepPlaylistDetailFragment_to_sleepPlaylistFragment)
+        initListeners()
+    }
+
+    private fun initListeners() = with(binding) {
+        llSleepMainPlaylist.setOnClickListener { findNavController().navigate(R.id.action_sleepPlaylistDetailFragment_to_sleepPlaylistFragment) }
+        ivSleepMainPlayToggle.setOnClickListener {
+            if(mediaPlayer == null) {
+                // 맨 처음 재생
+                mediaPlayer = MediaPlayer.create(context, R.raw.sleep_classic_gymnopedie_no1)
+                mediaPlayer?.start()
+                isPlaying = true
+                ivSleepMainPlayToggle.setImageResource(R.drawable.ic_pause)
+            } else {
+                if(isPlaying) {
+                    // 멈추기
+                    mediaPlayer?.pause()
+                    isPlaying = false
+                    ivSleepMainPlayToggle.setImageResource(R.drawable.ic_play_no_background)
+                } else {
+                    mediaPlayer?.start()
+                    isPlaying = true
+                    ivSleepMainPlayToggle.setImageResource(R.drawable.ic_pause)
+                }
+            }
         }
     }
 
