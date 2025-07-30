7120f7c370cd217a570dc3bfc4ea84b2328d32e1
se05503 <seyeongb18@gmail.com>
2025-07-30 16:18:26 +0900

    [test] 사용자가 잠든 시간, 마지막 사용 앱 추적 - UsageStatsManager API 사용 - 시간대: 전날 오후 8시 ~ 오늘 오전 4시 - 마지막 사용 앱, 마지막 사용 시간 추적

--- app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepReportFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/sleep/SleepReportFragment.kt
@@ -1,7 +1,11 @@
 package com.example.lifemaster.presentation.home.sleep
 
+import android.app.usage.UsageEvents
+import android.app.usage.UsageStatsManager
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.lifemaster.R
@@ -10,10 +14,14 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import java.util.Calendar
+import java.util.Date
 
 class SleepReportFragment : Fragment(R.layout.fragment_sleep_report) {
 
     private lateinit var binding: FragmentSleepReportBinding
+    private var lastUsedApp: String? = null // 마지막으로 사용한 앱
+    private var lastUsedTime: Long = 0L // 마지막 사용 시간 = 핸드폰 화면을 끈 시간
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -55,8 +63,8 @@ class SleepReportFragment : Fragment(R.layout.fragment_sleep_report) {
         lineDataSet.setCircleColor(Color.parseColor("#927448")) // 점 색상
         lineDataSet.circleRadius = 4f // 점 크기
         lineDataSet.setDrawValues(false) // 값이 안보이게 하기
-        //        lineDataSet.valueTextColor = Color.parseColor("#927448") // 값 색상
-        //        lineDataSet.setDrawFilled(true) // 선 아래 영역 채우기
+//        lineDataSet.valueTextColor = Color.parseColor("#927448") // 값 색상
+//        lineDataSet.setDrawFilled(true) // 선 아래 영역 채우기
 
         // 여러 개의 line 을 담는 전체 그래프 데이터
         val lineData = LineData(lineDataSet)
@@ -82,6 +90,31 @@ class SleepReportFragment : Fragment(R.layout.fragment_sleep_report) {
         // 점 클릭 시 나타나는 통계 세부 정보
         val markerView = SleepReportMarkerView(requireContext(), R.layout.layout_sleep_report_marker_view)
         lineChartSleepReportGraph.marker = markerView
+
+        // 사용자가 잠든 시간 추적하기
+        val usageStatsManager = requireContext().getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val calendar = Calendar.getInstance().apply {
+            // 오늘 날짜
+            set(Calendar.HOUR_OF_DAY, 4)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val endTime = calendar.timeInMillis // 새벽 4시
+        calendar.add(Calendar.HOUR_OF_DAY, -8)
+        val startTime = calendar.timeInMillis // 오후 8시
+
+        val event = UsageEvents.Event()
+
+        val usageEvents = usageStatsManager.queryEvents(startTime, endTime) // 전날 오후 8시 ~ 오늘 새벽 4시까지의 핸드폰 이용 내역 조회
+        while (usageEvents.hasNextEvent()) {
+            // while 문을 통해 해당 시간대의 마지막 핸드폰 사용 시간 추적 + 업데이트
+            usageEvents.getNextEvent(event) // 다음 이벤트를 변수에 저장
+            if(event.eventType == UsageEvents.Event.ACTIVITY_STOPPED && event.packageName != "com.sec.android.app.launcher") lastUsedApp = event.packageName // 전날 오후 8시 ~ 오늘 새벽 4시 중 마지막으로 사용한 앱 추적
+            else if(event.eventType == UsageEvents.Event.SCREEN_NON_INTERACTIVE) lastUsedTime = event.timeStamp // 전날 오후 8시 ~ 오늘 새벽 4시 중 마지막으로 화면을 끈 시각 추적
+        }
+
+        Log.e("수면 정보 추적", "마지막 사용 앱: $lastUsedApp, 화면 끈 시각: ${Date(lastUsedTime)}")
     }
 
 }
