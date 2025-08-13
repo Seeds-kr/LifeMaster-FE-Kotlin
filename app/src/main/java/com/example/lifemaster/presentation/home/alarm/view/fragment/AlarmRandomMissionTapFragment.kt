d5c81b4c8422dc796fc520bbce680eeb97dd878c
se05503 <seyeongb18@gmail.com>
2025-08-13 21:53:53 +0900

    [test] 랜덤미션(따라누르기) 초기 탭 상태 변경 1. 랜덤으로 10개(중복 포함) 탭 상태 변경 2. 카운트를 위한 뷰 추가 3. coroutine 사용 - launch 코루틴 빌더 사용 - lifecycleScope로 코루틴 실행범위 지정 및 생명주기 관리 - delay 지연 함수를 통해 카운트 뷰 UI 업데이트

--- app/src/main/java/com/example/lifemaster/presentation/home/alarm/view/fragment/AlarmRandomMissionTapFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/alarm/view/fragment/AlarmRandomMissionTapFragment.kt
@@ -2,10 +2,15 @@ package com.example.lifemaster.presentation.home.alarm.view.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.lifemaster.R
 import com.example.lifemaster.databinding.FragmentAlarmRandomMissionTapBinding
 import com.google.android.material.card.MaterialCardView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
 class AlarmRandomMissionTapFragment : Fragment(R.layout.fragment_alarm_random_mission_tap) {
@@ -48,16 +53,55 @@ class AlarmRandomMissionTapFragment : Fragment(R.layout.fragment_alarm_random_mi
             cvAlarmRandomMissionTap24,
             cvAlarmRandomMissionTap25
         )
+        lifecycleScope.launch {
+            repeat(10) {
+                val i = Random.nextInt(0, 25) // 0 ~ 24 (중복 허용)
+                taps[i].apply {
+                    isSelected = true
+                    setCardBackgroundColor(
+                        resources.getColor(
+                            R.color.alarm_primary,
+                            context?.theme
+                        )
+                    )
+                }
+            }
+            delay(1000)
+            tvAlarmRandomMissionTapCount.text = "2"
+            delay(1000)
+            tvAlarmRandomMissionTapCount.text = "1"
+            delay(1000)
+            tvAlarmRandomMissionTapCount.isVisible = false
+            for (tap in taps) {
+                tap.isSelected = false
+                tap.setCardBackgroundColor(
+                    resources.getColor(
+                        R.color.light_gray_100,
+                        context?.theme
+                    )
+                )
+            }
+        }
     }
 
     private fun initListeners() = with(binding) {
-        for(tap in taps) {
+        for (tap in taps) {
             tap.setOnClickListener {
                 tap.isSelected = !tap.isSelected
-                if(tap.isSelected) {
-                    tap.setCardBackgroundColor(resources.getColor(R.color.alarm_primary, context?.theme))
+                if (tap.isSelected) {
+                    tap.setCardBackgroundColor(
+                        resources.getColor(
+                            R.color.alarm_primary,
+                            context?.theme
+                        )
+                    )
                 } else {
-                    tap.setCardBackgroundColor(resources.getColor(R.color.light_gray_100, context?.theme))
+                    tap.setCardBackgroundColor(
+                        resources.getColor(
+                            R.color.light_gray_100,
+                            context?.theme
+                        )
+                    )
                 }
             }
         }
