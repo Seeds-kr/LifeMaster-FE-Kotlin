df6202eb7e3dced5ef932d0456c3cbf1c611cb4f
se05503 <seyeongb18@gmail.com>
2025-08-13 13:38:50 +0900

    [feat] 랜덤미션(따라누르기) 유저 터치 이벤트 받기 - style 수정 → style 속성은 XML에서만 적용되고, 런타임에서 직접 변경할 수 없음 - isSelected 상태를 받아서 card background 변경

---
+++ app/src/main/java/com/example/lifemaster/presentation/home/alarm/view/fragment/AlarmRandomMissionTapFragment.kt
@@ -0,0 +1,65 @@
+package com.example.lifemaster.presentation.home.alarm.view.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.lifemaster.R
+import com.example.lifemaster.databinding.FragmentAlarmRandomMissionTapBinding
+import com.google.android.material.card.MaterialCardView
+
+
+class AlarmRandomMissionTapFragment : Fragment(R.layout.fragment_alarm_random_mission_tap) {
+
+    private lateinit var binding: FragmentAlarmRandomMissionTapBinding
+    private lateinit var taps: List<MaterialCardView>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAlarmRandomMissionTapBinding.bind(view)
+        initViews()
+        initListeners()
+    }
+
+    private fun initViews() = with(binding) {
+        taps = listOf(
+            cvAlarmRandomMissionTap1,
+            cvAlarmRandomMissionTap2,
+            cvAlarmRandomMissionTap3,
+            cvAlarmRandomMissionTap4,
+            cvAlarmRandomMissionTap5,
+            cvAlarmRandomMissionTap6,
+            cvAlarmRandomMissionTap7,
+            cvAlarmRandomMissionTap8,
+            cvAlarmRandomMissionTap9,
+            cvAlarmRandomMissionTap10,
+            cvAlarmRandomMissionTap11,
+            cvAlarmRandomMissionTap12,
+            cvAlarmRandomMissionTap13,
+            cvAlarmRandomMissionTap14,
+            cvAlarmRandomMissionTap15,
+            cvAlarmRandomMissionTap16,
+            cvAlarmRandomMissionTap17,
+            cvAlarmRandomMissionTap18,
+            cvAlarmRandomMissionTap19,
+            cvAlarmRandomMissionTap20,
+            cvAlarmRandomMissionTap21,
+            cvAlarmRandomMissionTap22,
+            cvAlarmRandomMissionTap23,
+            cvAlarmRandomMissionTap24,
+            cvAlarmRandomMissionTap25
+        )
+    }
+
+    private fun initListeners() = with(binding) {
+        for(tap in taps) {
+            tap.setOnClickListener {
+                tap.isSelected = !tap.isSelected
+                if(tap.isSelected) {
+                    tap.setCardBackgroundColor(resources.getColor(R.color.alarm_primary, context?.theme))
+                } else {
+                    tap.setCardBackgroundColor(resources.getColor(R.color.light_gray_100, context?.theme))
+                }
+            }
+        }
+    }
+}
