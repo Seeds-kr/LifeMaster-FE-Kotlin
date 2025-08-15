13b2fa244ec17cdd6dc5c21785614454167b017a
se05503 <seyeongb18@gmail.com>
2025-08-15 22:34:05 +0900

    [feat] 랜덤 미션 페이지 넘어가기 1. 시도 방법 - singleton의 newInstance → FragmentManager 사용해야 하는데, 현재 Navigation 구조에서는 어려움 - viewpager2 + fragmentStateAdapter → 스와이프 기능 필요x, fragment 상태 유지 필요x - navigation → 독립적인 프래그먼트 인스턴스 생성, 백스택 관리 용이 2. 선택 방법(navigation) - nav_graph 에서 argument 지정 3. 위젯 추가 및 뒤로가기 비활성화

--- app/src/main/java/com/example/lifemaster/presentation/home/alarm/view/fragment/AlarmRandomMissionTapFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/alarm/view/fragment/AlarmRandomMissionTapFragment.kt
@@ -3,9 +3,14 @@ package com.example.lifemaster.presentation.home.alarm.view.fragment
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.example.lifemaster.R
 import com.example.lifemaster.databinding.FragmentAlarmRandomMissionTapBinding
 import com.google.android.material.card.MaterialCardView
@@ -20,15 +25,24 @@ class AlarmRandomMissionTapFragment : Fragment(R.layout.fragment_alarm_random_mi
     private lateinit var taps: List<MaterialCardView>
     private var answerTapPositions: MutableSet<Int> = hashSetOf()
     private var userTapPositions: MutableSet<Int> = hashSetOf()
+    private var currentPage: Int = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAlarmRandomMissionTapBinding.bind(view)
+        currentPage = arguments?.getInt("currentPageNum")!! // 파라미터 전달 받기
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // 뒤로가기 버튼 비활성화
+            }
+        })
         initViews()
         initListeners()
     }
 
     private fun initViews() = with(binding) {
+        Log.e("TTEST", ""+currentPage)
+        tvAlarmRandomMissionTapPage.text = "${currentPage}/3"
         taps = listOf(
             cvAlarmRandomMissionTap1,
             cvAlarmRandomMissionTap2,
@@ -116,8 +130,21 @@ class AlarmRandomMissionTapFragment : Fragment(R.layout.fragment_alarm_random_mi
             taps.forEachIndexed { position, tap ->
                 if(tap.isSelected) userTapPositions.add(position) else userTapPositions.remove(position)
             }
-            if(answerTapPositions.equals(userTapPositions)) Log.e("TTEST", "GOOD")
-            else Log.e("TTEST", "BAD")
+            if(answerTapPositions.equals(userTapPositions)) {
+                if(currentPage == 3) {
+                    Toast.makeText(context, "수고하셨습니다!", Toast.LENGTH_SHORT).show()
+                } else {
+                    findNavController().navigate(
+                        R.id.alarmRandomMissionTapFragment,
+                        bundleOf("currentPageNum" to ++currentPage),
+                        NavOptions.Builder()
+                            .setLaunchSingleTop(true) // 최상단이 같은 프래그먼트인 경우 쌓지 않고 교체함
+                            .build()
+                    )
+                }
+            }
+            else { Toast.makeText(context, "답이 틀렸습니다! 다시 입력해주세요!", Toast.LENGTH_SHORT).show() }
         }
     }
+
 }
