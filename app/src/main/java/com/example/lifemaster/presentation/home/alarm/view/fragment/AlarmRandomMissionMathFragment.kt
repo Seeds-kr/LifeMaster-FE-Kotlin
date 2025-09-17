09803279f213b664c73c8eaf771efe3896006723
se05503 <seyeongb18@gmail.com>
2025-09-17 16:06:54 +0900

    [feat] 수학 문제 기능 2차 구현 - 수면 기록을 위한 백엔드 연결 - 랜덤 미션 끝난 후 화면 이동했을 때 뒤로가면 앱이 꺼지도록 하기 위해 popUp 설정

--- app/src/main/java/com/example/lifemaster/presentation/home/alarm/view/fragment/AlarmRandomMissionMathFragment.kt
+++ app/src/main/java/com/example/lifemaster/presentation/home/alarm/view/fragment/AlarmRandomMissionMathFragment.kt
@@ -1,11 +1,15 @@
 package com.example.lifemaster.presentation.home.alarm.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.example.lifemaster.R
 import com.example.lifemaster.databinding.FragmentAlarmRandomMissionMathBinding
 import com.example.lifemaster.network.RetrofitInstance
@@ -14,6 +18,16 @@ import com.example.lifemaster.presentation.home.alarm.viewmodel.AlarmViewModelFa
 import com.google.android.material.card.MaterialCardView
 import java.time.LocalDate
 import java.time.LocalTime
+import android.util.Log
+import com.example.lifemaster.presentation.Constants
+import com.example.lifemaster.presentation.home.alarm.view.service.AlarmService
+import com.example.lifemaster.presentation.home.sleep.model.AlarmInfo
+import com.example.lifemaster.presentation.home.sleep.model.AlarmSettingInfo
+import com.example.lifemaster.presentation.home.sleep.model.Result
+import com.example.lifemaster.presentation.home.sleep.model.SleepRequest
+import com.example.lifemaster.presentation.home.sleep.viewmodel.SleepViewModel
+import com.example.lifemaster.presentation.home.sleep.viewmodel.SleepViewModelFactory
+import java.time.Instant
 
 class AlarmRandomMissionMathFragment : Fragment(R.layout.fragment_alarm_random_mission_math) {
 
@@ -21,9 +35,19 @@ class AlarmRandomMissionMathFragment : Fragment(R.layout.fragment_alarm_random_m
     private val alarmViewModel: AlarmViewModel by activityViewModels(
         factoryProducer = { AlarmViewModelFactory(RetrofitInstance.networkService) }
     )
+    private val sleepViewModel: SleepViewModel by activityViewModels(
+        factoryProducer = {SleepViewModelFactory(RetrofitInstance.networkService)}
+    )
+
     private lateinit var numberPadList: List<MaterialCardView> // 1 ~ 9
 
+    private var currentPage: Int = 1
     private var correctAnswer: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("Lifecycle", "onCreate")
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,20 +63,113 @@ class AlarmRandomMissionMathFragment : Fragment(R.layout.fragment_alarm_random_m
     }
 
     private fun initObservers() = with(binding) {
+        // alarm
         alarmViewModel.mathProblemInfo.observe(viewLifecycleOwner) { mathProblemInfo ->
             tvAlarmRandomMissionMathQuestion.text = mathProblemInfo.question
             correctAnswer = mathProblemInfo.correctAnswer
         }
+        // sleep
+        sleepViewModel.userSleepRecordList.observe(viewLifecycleOwner) { result ->
+                when(result) {
+                    is Result.Success -> {
+                        alarmViewModel.alarmDismissedAt = System.currentTimeMillis()
+                        val data = result.data
+                        val todayRecord = data.find { it.sleepDate == LocalDate.now().toString() }
+                        if(todayRecord == null) {
+                            // POST
+                            sleepViewModel.registerUserSleepInfo(
+                                sleepRequest = SleepRequest(
+                                    userId = Constants.USER_ID,
+                                    sleepDate = LocalDate.now().toString(),
+                                    sleepStart = Instant.ofEpochMilli(sleepViewModel.rawSleepTime ?: 0L).toString(),
+                                    sleepEnd =  Instant.ofEpochMilli(alarmViewModel.alarmDismissedAt ?: 0L).toString(),
+                                    sleepMood = "GOOD",
+                                    alarmInfo = AlarmInfo(
+                                        isWakeUpAlarmSet = true,
+                                        alarmSettings = AlarmSettingInfo(
+                                            alarmSnoozeCnt = 0, // TODO: 실제 알람 데이터로 변경하기
+                                            timeToWakeUp = 0, // TODO: 실제 알람 데이터로 변경하기
+                                            antiSleepMode = false // TODO: 실제 알람 데이터로 변경하기
+                                        )
+                                    )
+                                )
+                            )
+                        } else {
+                            // PATCH
+                            sleepViewModel.updateUserSleepInfo(
+                                sleepRequest = SleepRequest(
+                                    userId = Constants.USER_ID,
+                                    sleepDate = LocalDate.now().toString(),
+                                    sleepStart = Instant.ofEpochMilli(sleepViewModel.rawSleepTime ?: 0L).toString(),
+                                    sleepEnd =  Instant.ofEpochMilli(alarmViewModel.alarmDismissedAt ?: 0L).toString(),
+                                    sleepMood = "GOOD",
+                                    alarmInfo = AlarmInfo(
+                                        isWakeUpAlarmSet = true,
+                                        alarmSettings = AlarmSettingInfo(
+                                            alarmSnoozeCnt = 0, // TODO: 실제 알람 데이터로 변경하기
+                                            timeToWakeUp = 0, // TODO: 실제 알람 데이터로 변경하기
+                                            antiSleepMode = false // TODO: 실제 알람 데이터로 변경하기
+                                        )
+                                    )
+                                )
+                            )
+                        }
+                    }
+                    is Result.Error -> {
+                        Toast.makeText(context, "네트워크 연결이 불안정합니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    is Result.Loading -> {}
+                }
+            }
+
+        sleepViewModel.isUserSleepRecordGenerated.observe(viewLifecycleOwner) { event ->
+            event.getDataIfNotHandled()?.let { isSuccess ->
+                if (isSuccess) {
+                    Toast.makeText(context, "수면 기록 전송이 성공했습니다", Toast.LENGTH_SHORT).show()
+                    // 알람 소리 멈추기
+                    val serviceIntent = Intent(context, AlarmService::class.java)
+                    requireContext().stopService(serviceIntent)
+                    findNavController().navigate(
+                        R.id.action_alarmRandomMissionMathFragment_to_alarmListFragment,
+                        bundleOf("origin" to "alarm_random_mission") // TODO: value 값 변경해야하는가? (랜덤 미션 종류별로 분기를 쳐야하는가?)
+                    )
+                } else {
+                    Toast.makeText(context, "네트워크 연결이 불안정합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        sleepViewModel.userSleepUpdatedRecord.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Result.Success -> {
+                    Toast.makeText(context, "수면 기록이 업데이트 되었습니다", Toast.LENGTH_SHORT).show()
+                    // 알람 소리 멈추기
+                    val serviceIntent = Intent(context, AlarmService::class.java)
+                    requireContext().stopService(serviceIntent)
+                    findNavController().navigate(
+                        R.id.action_alarmRandomMissionMathFragment_to_alarmListFragment,
+                        bundleOf("origin" to "alarm_random_mission") // TODO: value 값 변경해야하는가? (랜덤 미션 종류별로 분기를 쳐야하는가?)
+                    )
+                }
+                is Result.Error -> {
+                    Toast.makeText(context, "네트워크 연결이 불안정합니다.", Toast.LENGTH_SHORT).show()
+                }
+                is Result.Loading -> {}
+            }
+        }
+
     }
 
     private fun initViews() = with(binding) {
         // late initialization
         numberPadList = listOf(cvMathNumber1, cvMathNumber2, cvMathNumber3, cvMathNumber4, cvMathNumber5, cvMathNumber6, cvMathNumber7, cvMathNumber8, cvMathNumber9)
+        currentPage = arguments?.getInt(PAGE_KEY)!!
 
         // UI binding
         tvAlarmRandomMissionMathDate.text = "${LocalDate.now().year}년 ${LocalDate.now().monthValue}월 ${LocalDate.now().dayOfMonth}일"
         tvAlarmRandomMissionMathTime.text = "${LocalTime.now().hour}:${LocalTime.now().minute}"
         tvAlarmRandomMissionMathAmPm.text = if(LocalTime.now().hour in 0..11) "am" else "pm"
+        tvAlarmRandomMissionMathPage.text = "${currentPage}/${TOTAL_PAGE_NUM}"
     }
 
     private fun initListeners() = with(binding) {
@@ -96,9 +213,15 @@ class AlarmRandomMissionMathFragment : Fragment(R.layout.fragment_alarm_random_m
             val userAnswer = tvAlarmRandomMissionUserAnswer.text.toString().toIntOrNull()
             if(userAnswer == null) {
                 Toast.makeText(context, "답을 입력해주세요", Toast.LENGTH_SHORT).show()
-            } else if(userAnswer == correctAnswer) {
-                // TODO: 다음 페이지로 넘어가기 + 문제 갱신하기 + 답변 칸 지우기
+            } else if(userAnswer == correctAnswer && currentPage < TOTAL_PAGE_NUM) {
+                findNavController().navigate(
+                    R.id.alarmRandomMissionMathFragment,
+                    bundleOf(PAGE_KEY to currentPage+1),
+                    NavOptions.Builder().setLaunchSingleTop(true).build() // 최상단에 같은 프래그먼트가 있으면 쌓이지 않게 하기
+                )
                 Toast.makeText(context, "답이 맞았습니다!", Toast.LENGTH_SHORT).show()
+            } else if(userAnswer == correctAnswer && currentPage == TOTAL_PAGE_NUM) {
+                sleepViewModel.getUserSleepInfo(Constants.USER_ID)
             } else {
                 Toast.makeText(context, "답이 틀렸습니다!", Toast.LENGTH_SHORT).show()
             }
@@ -109,6 +232,8 @@ class AlarmRandomMissionMathFragment : Fragment(R.layout.fragment_alarm_random_m
         private const val MATH_LEVEL_HIGH = "상"
         private const val MATH_LEVEL_MEDIUM = "중"
         private const val MATH_LEVEL_LOW = "하"
+        private const val PAGE_KEY = "currentPageNum"
+        private const val TOTAL_PAGE_NUM = 3
     }
 
 }
